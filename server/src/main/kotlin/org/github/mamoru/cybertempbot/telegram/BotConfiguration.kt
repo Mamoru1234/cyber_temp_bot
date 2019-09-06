0d93b8718cbeb83a1cfe41cc2f028e243adf2e54
Alexei Gontar <f.gontar5@gmail.com>
2019-09-06 17:41:06 +0300

    Revert "Separate telegram module"
    This reverts commit abb2eb53

--- server/src/main/kotlin/org/github/mamoru/cybertempbot/telegram/BotConfiguration.kt
+++ server/src/main/kotlin/org/github/mamoru/cybertempbot/telegram/BotConfiguration.kt
@@ -3,11 +3,9 @@ package org.github.mamoru.cybertempbot.telegram
 import com.pengrad.telegrambot.TelegramBot
 import org.github.mamoru.cybertempbot.telegram.properties.TelegramBotProperties
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@ComponentScan
 class BotConfiguration(private val props: TelegramBotProperties) {
     @Bean
     fun tgBot(): TelegramBot {
